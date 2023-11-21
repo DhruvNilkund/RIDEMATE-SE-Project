1dc85638a12f641615ce64d1e3de55e3473ecd54
DARSHANv17 <129108514+DARSHANv17@users.noreply.github.com>
2023-11-21 18:56:00 +0530

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,27 +2,38 @@ const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
 
+// Define a "login" strategy for Passport
 passport.use(
     "login",
     new localStrategy({
-            usernameField: "email",
-            passwordField: "password",
-        },
-        async(email, password, done) => {
-            try {
-                const user = await User.findOne({ email });
-                if (!user) {
-                    return done(null, false, { message: "User not found" });
-                }
+        // Specify the fields used for username and password in the login form
+        usernameField: "email",
+        passwordField: "password",
+    },
+    // Callback function called during the login process
+    async(email, password, done) => {
+        try {
+            // Find a user with the provided email in the database
+            const user = await User.findOne({ email });
 
-                const validate = await user.isValidPassword(password);
-                if (!validate) {
-                    return done(null, false, { message: "Wrong Password" });
-                }
-                return done(null, user, { message: "Logged in Successfully" });
-            } catch (error) {
-                return done(error);
+            // If no user is found, return an error message
+            if (!user) {
+                return done(null, false, { message: "User not found" });
             }
+
+            // Validate the user's password
+            const validate = await user.isValidPassword(password);
+
+            // If the password is invalid, return an error message
+            if (!validate) {
+                return done(null, false, { message: "Wrong Password" });
+            }
+
+            // If both email and password are valid, return the user and a success message
+            return done(null, user, { message: "Logged in Successfully" });
+        } catch (error) {
+            // If an error occurs during the login process, return the error
+            return done(error);
         }
-    )
+    })
 );
