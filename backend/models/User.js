80dce2a90d9ac1dcf2c95cb748c82901cbc3353c
DARSHANv17 <129108514+DARSHANv17@users.noreply.github.com>
2023-11-21 18:58:21 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,33 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Define the schema for the 'users' collection
 const UserSchema = new Schema({
+    // User's name, required field
     name: {
         type: String,
         required: true,
     },
+    // User's email, required field
     email: {
         type: String,
         required: true,
     },
+    // User's password, required field
     password: {
         type: String,
         required: true,
     },
+    // User's mobile number, required field
     mobile: {
         type: String,
         required: true,
     },
+    // User's gender, required field
     gender: {
         type: String,
         required: true,
     },
+    // User's date of birth, optional field
     dob: {
         type: Date,
         required: false,
     },
 });
 
+// Create a Mongoose model named 'user' based on the defined schema
 const User = mongoose.model("user", UserSchema);
 
+// Export the 'user' model to be used in other parts of the application
 module.exports = User;
