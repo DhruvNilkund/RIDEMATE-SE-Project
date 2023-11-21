f9755c23c9d7cb606bfd92a5c5f8c0473cbbc4ef
DARSHANv17 <129108514+DARSHANv17@users.noreply.github.com>
2023-11-21 18:52:41 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,5 @@
+// Export MongoDB connection URI for database access
 module.exports = {
+    // MongoDB connection URI includes the username, password, cluster URL, and other parameters
     MongoURI: "mongodb+srv://AdityaNew:123@cluster0-rsoqu.mongodb.net/test?retryWrites=true&w=majority"
-}
+};
